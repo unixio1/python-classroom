b212e84c3d9c423061c6c7eba7f7aa530245fa9c
unixio1 <unaxag01@gmail.com>
2025-08-09 15:38:49 +0200

    feat: Add basic terminal which runs python code and logs it using pyodide

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,21 @@
+import { useEffect, useState } from "react"
+import Terminal from "./components/terminal"
+import { loadPyodide, type PyodideAPI } from "pyodide";
 export default function App() {
+  const [pyodide, setPyodide] = useState<PyodideAPI | undefined>();
+  const [editorCode, setEditorCode] = useState('');
+  useEffect(() => {
+     loadPyodide({
+        indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.28.1/full'
+      }).then(pyodide => setPyodide(pyodide))
+  }, []);
+  const onRunCode = () => {
+    pyodide?.runPython(editorCode)
+  };
   return (
     <div>
+      <Terminal setEditorCode={setEditorCode}/>
+      <button onClick={onRunCode}>Run code</button>
     </div>
   )  
 }
