117577765d9ffc1d263f96a5fa95d984a6a6cd8c
unixio1 <unaxag01@gmail.com>
2025-08-11 09:57:32 +0200

    feat: Integrate the stdout component to the main app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,32 @@
 import { useEffect, useState } from "react"
 import Terminal from "./components/terminal"
 import { loadPyodide, type PyodideAPI } from "pyodide";
+import CodeStdout from "./components/codeStdout";
 export default function App() {
   const [pyodide, setPyodide] = useState<PyodideAPI | undefined>();
   const [editorCode, setEditorCode] = useState('');
+  const [codeLogs, setCodeLogs] = useState<string[]>([]);
   useEffect(() => {
      loadPyodide({
         indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.28.1/full'
-      }).then(pyodide => setPyodide(pyodide))
+      }).then(pyodide => {
+        pyodide.setStdout({
+          batched: (msg) => {
+            setCodeLogs(logs => [...logs, msg]);
+          }
+        })
+        setPyodide(pyodide)
+      })
   }, []);
-  const onRunCode = () => {
-    pyodide?.runPython(editorCode)
+  const onRunCode = async () => {
+    setCodeLogs([]);
+    await pyodide?.runPythonAsync(editorCode);
   };
   return (
     <div>
       <Terminal setEditorCode={setEditorCode}/>
       <button onClick={onRunCode}>Run code</button>
+      <CodeStdout logs={codeLogs}></CodeStdout>
     </div>
   )  
 }
