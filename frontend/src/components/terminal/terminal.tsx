1a3553936f44d3fba2cc261569a6eea5ccf7f77b
unixio1 <unaxag01@gmail.com>
2025-08-12 11:07:34 +0200

    feat: Implement basic terminal-like styles to the terminal component

--- frontend/src/components/terminal/terminal.tsx
+++ frontend/src/components/terminal/terminal.tsx
@@ -1,12 +1,22 @@
+import styles from './terminal.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
 
 export default function Terminal(props: TerminalProps){
-  return <div>
-    <h2>Terminal</h2>
-    <textarea 
+  return <div className={styles.terminalWrapper}>
+    <div className={styles.utilities}>
+      <h2>Terminal</h2>
+      <div className={styles.buttons}>
+        <button onClick={(_e) => props.onRunCode()} className={styles.run}>
+          <FontAwesomeIcon icon={faPlay} />
+        </button>
+        </div>
+      </div>
+    <textarea
+      className={styles.terminalText}
       onChange={(e) => props.setEditorCode(e.target.value)}
     >
     </textarea>
-    <button onClick={props.onRunCode}>Run code</button>
   </div>
 }
 
