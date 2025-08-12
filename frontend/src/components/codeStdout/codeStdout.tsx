37fe3c78697270ff11adc45eb655ec47c2a92c6c
unixio1 <unaxag01@gmail.com>
2025-08-12 11:24:59 +0200

    feat: Add basic code logging styles

--- frontend/src/components/codeStdout/codeStdout.tsx
+++ frontend/src/components/codeStdout/codeStdout.tsx
@@ -1,10 +1,11 @@
-
+import styles from './codeStdout.module.scss';
 export default function CodeStdout(props: CodeStdoutProps){
-  return <>
+  return <div className={styles.output}>
+  <span>Logs</span><br/>
   {props.logs && (
     <code>{props.logs.join('\n')}</code>
   )}
-  </>
+  </div>
 }
 
 interface CodeStdoutProps{
